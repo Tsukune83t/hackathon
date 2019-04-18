4e7aacc8173af0e98db3ce12b959d9eb76695cb7
Tsukune83t <nico83t@gmail.com>
2019-04-18 17:15:33 +0200

    conflit réglé

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import Api from './components/content/Api';
 
 export default class Content extends Component {
   render() {
     return (
       <div>
-        
+        <Api />
       </div>
     )
   }
