d5cce406728c246f564caa1872a058c55aff2b68
hafid <cyberhafid@gmail.com>
2019-04-18 23:10:53 +0200

    misa ajour

--- src/pages/Quete.jsx
+++ src/pages/Quete.jsx
@@ -3,7 +3,20 @@ import { Row, Col ,Container} from 'reactstrap';
 import MyNavbar from '../components/header/Navbar';
 import './que.css';
 import { Button } from 'reactstrap';
+
 export default class Quete extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      egg: 0
+    };
+    this.addEgg = this.addEgg.bind(this);
+  }
+  addEgg() {
+    this.setState(state => ({
+      egg: state.egg + 1 
+    }));
+  }
   render() {
     return (
   
@@ -13,16 +26,23 @@ export default class Quete extends Component {
 
         <div  className="corps">
           <Row className="title" >
-            <Col sm="12">bonjour </Col>
+            <Col sm="12"><p>A cliqué {this.state.egg} fois</p> </Col>
           </Row >
           <Row className="centre">
-            <Col sm="6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). </Col>
+            <Col sm="6">Le plus petit des deux?. </Col>
      
-            <Col sm="6">  <img src="https://la-cascade.io/content/images/2015/06/kiwi.svg" thumbnail/> </Col>
+            <Col sm="6">  <img src="https://images.innoveduc.fr/easter_hackathon/6438864-dc94c42f-acac-44ff-8804-85a37f06262d.jpeg" thumbnail/> </Col>
+
+          </Row>
+          <Row className="question">
+
+            <Col sm="3"> <Button onClick={this.addEgg} color="warning" size="lg">Large Button</Button>{' '}</Col>
+     
+            <Col sm="3">  <Button color="warning" size="lg">Large Button</Button>{' '}  </Col>
+            <Col sm="6">    </Col>
           </Row>
           <Row className="pied">
             <Col sm="12">
-              <Button outline color="warning">warning</Button>{' '}
             
             </Col>
           </Row>
