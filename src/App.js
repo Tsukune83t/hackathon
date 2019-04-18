95a3f2a2ec92163ff70344022fb1cdf301ff9dd5
Miatack <miatack529@gmail.com>
2019-04-19 00:52:23 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Content from './components/content/Content.jsx'
-
 import Routeur from './components/Routeur';
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
+        <Header />
         <Content />
-=======
-      
-        
         <React.Fragment>
-
           <Routeur />
         </React.Fragment>
->>>>>>> 2a74224f5264e2f53c6ce2823f5322b51b545883
+        <Footer />
       </div>
     );
   }
